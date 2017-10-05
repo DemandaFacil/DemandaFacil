6ab228669817e375e052bbd14a4a7dc90e95147f
rastamarco <31351424+rastamarco@users.noreply.github.com>
2017-10-04 22:05:30 -0300

    Classe Gráfico

---
+++ DemandaFacil/src/controllers/Main.java
@@ -0,0 +1,9 @@
+package controllers;
+
+public class Main {
+    public static void main(String[] args) {
+        GeradorGrafico x = new GeradorGrafico();
+        x.criaGrafico();
+        x.setVisible(true);
+    }
+}
