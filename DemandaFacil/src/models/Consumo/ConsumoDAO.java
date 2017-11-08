34d7bc2f38f1b3c9fd89dcda2a933012049b66d4
unknown <nicolasaoki@alunos.utfpr.edu.br>
2017-11-08 20:50:04 -0200

    inicio media ponderada

---
+++ DemandaFacil/src/models/Consumo/ConsumoDAO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models.Consumo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Aluno
+ */
+public class ConsumoDAO {
+    public ArrayList listaConsumo(int mesInicio,int mesFim) throws Exception{
+        try{
+            
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    
+}
+
