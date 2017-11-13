c7c6d10b316cb5cd110e2b047736c1adb85cc84b
rastamarco <31351424+rastamarco@users.noreply.github.com>
2017-11-12 23:14:58 -0200

    Poucas Alterações na classe gráfico com o calculo de desvio

--- DemandaFacil/src/controllers/grafico/Teste.java
+++ DemandaFacil/src/controllers/grafico/Teste.java
@@ -334,9 +334,6 @@ public class Teste extends javax.swing.JFrame {
         return qtde2;
     }
     
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
